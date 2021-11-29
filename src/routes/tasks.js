234a2af999832895b788fee94391df20164566b8
lozanomasuno <lozanomasuno@gmail.com>
2021-11-29 14:00:10 -0500

    put

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -7,6 +7,24 @@ router.get('/', (req, res)=>{
     res.json(tasks);
 });
 
+router.put('/:id', (req, res)=>{
+    const {id} = req.params;
+    const { title, importance, project, analist} = req.body;
+    if( title && importance && project &&  project) {
+      _.each(tasks, (task, i)=> {
+         if(task.id == id){
+            task.title = title;
+            task.importance = importance;
+            task.project = project;
+            task.analist = analist;            
+         }        
+      });      
+      res.json(tasks);
+    } else {
+        res.status(500).json({error: 'there was an error.'});
+    }   
+});
+
 router.post('/', (req, res)=>{
     const { title, importance, project, analist} = req.body;
     if(title && importance && project &&  analist){
