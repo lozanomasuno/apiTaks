e70cc9efd95f98fcb2020158e6ccfdbedf63e29f
lozanomasuno <lozanomasuno@gmail.com>
2021-11-29 12:50:37 -0500

    post method

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -7,4 +7,16 @@ router.get('/', (req, res)=>{
     res.json(tasks);
 });
 
+router.post('/', (req, res)=>{
+    const { title, importance, project, analist} = req.body;
+    if(title && importance && project &&  analist){
+        const id = tasks.length + 1;
+        const newTask = {...req.body, id};
+        tasks.push(newTask);
+        res.json(tasks);
+    }else{
+        res.send('wrong request');
+    }
+})
+
 module.exports = router;
